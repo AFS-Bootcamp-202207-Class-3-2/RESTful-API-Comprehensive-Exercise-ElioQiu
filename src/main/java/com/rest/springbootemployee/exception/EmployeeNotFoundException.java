f74a77f3d464cb5432fd97d5d326b304e4d7800f
ELIO QIU (ITA-ISDC-ISD-OOCLL/ZHA) <QIUEL3@OOCL.COM>
2022-07-27 17:17:27 +0800

    test: add findemployeeById test in the controller layer

--- src/main/java/com/rest/springbootemployee/exception/EmployeeNotFoundException.java
+++ src/main/java/com/rest/springbootemployee/exception/EmployeeNotFoundException.java
@@ -1,5 +1,8 @@
 package com.rest.springbootemployee.exception;
 
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class EmployeeNotFoundException extends RuntimeException{
     public EmployeeNotFoundException() {
         super("employee not found");
