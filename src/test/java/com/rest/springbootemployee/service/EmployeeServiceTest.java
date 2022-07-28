1485fb041123b75b955309dc6951165b4b866604
blank1024 <13380497335@163.com>
2022-07-28 23:29:36 +0800

    feat: use JPA implement service test

--- src/test/java/com/rest/springbootemployee/service/EmployeeServiceTest.java
+++ src/test/java/com/rest/springbootemployee/service/EmployeeServiceTest.java
@@ -1,16 +1,19 @@
 package com.rest.springbootemployee.service;
 
 import com.rest.springbootemployee.entity.Employee;
-import com.rest.springbootemployee.repository.EmployeeRepository;
+import com.rest.springbootemployee.repository.EmployeeJpaRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
@@ -20,7 +23,7 @@ import static org.mockito.Mockito.*;
 public class EmployeeServiceTest {
 
     @Mock
-    EmployeeRepository employeeRepository;
+    EmployeeJpaRepository employeeJpaRepository;
 
     @InjectMocks
     EmployeeService employeeService;
@@ -33,7 +36,7 @@ public class EmployeeServiceTest {
         Employee secondEmployee = new Employee(2, "Jack", 25, "male", 10000, 1);
         preparedEmployees.add(firstEmployee);
         preparedEmployees.add(secondEmployee);
-        given(employeeRepository.findAll()).willReturn(preparedEmployees);
+        given(employeeJpaRepository.findAll()).willReturn(preparedEmployees);
         //when
         List<Employee> employees = employeeService.findAll();
         //then
@@ -44,9 +47,9 @@ public class EmployeeServiceTest {
     @Test
     void should_update_only_age_and_salary_when_update_given_employee() {
         //given
-        Employee employeeToUpdate = new Employee(1, "Susan", 20, "female", 8000, 1);
-        Employee employeeAfterUpdate = new Employee(1, "Mike", 40, "male", 28000, 1);
-        given(employeeRepository.findById(1)).willReturn(employeeToUpdate);
+        Employee employeeToUpdate = new Employee(null, "Susan", 20, "female", 8000, 1);
+        Employee employeeAfterUpdate = new Employee(null, "Mike", 40, "male", 28000, 1);
+        given(employeeJpaRepository.findById(1)).willReturn(Optional.of(employeeToUpdate));
         //when
         Employee update = employeeService.update(1, employeeAfterUpdate);
         //then
@@ -59,12 +62,11 @@ public class EmployeeServiceTest {
     @Test
     void should_find_by_id_when_get_given_employee_and_id() {
         //given
-        Employee employeeById = new Employee(2, "Lucy", 22, "female", 8000, 1);
-        employeeRepository.addEmployee(employeeById);
-        employeeRepository.addEmployee(new Employee(1, "Mike", 22, "male", 8000, 1));
-        given(employeeRepository.findById(2)).willReturn(employeeById);
+        Employee employeeById =
+                new Employee(1, "Lucy", 22, "female", 8000, 1);
+        given(employeeJpaRepository.findById(1)).willReturn(Optional.of(employeeById));
         //when
-        Employee employee = employeeService.findById(2);
+        Employee employee = employeeService.findById(employeeById.getId());
         //then
         assertEquals(employeeById, employee);
     }
@@ -75,12 +77,12 @@ public class EmployeeServiceTest {
         List<Employee> maleEmployees = new ArrayList<>();
         Employee employeeByGender1 = new Employee(1, "Mike", 22, "male", 8000, 1);
         Employee employeeByGender2 = new Employee(3, "Jack", 22, "male", 8000, 2);
-        employeeRepository.addEmployee(employeeByGender1);
-        employeeRepository.addEmployee(employeeByGender2);
-        employeeRepository.addEmployee(new Employee(2, "Lucy", 22, "female", 8000, 1));
+        employeeJpaRepository.save(employeeByGender1);
+        employeeJpaRepository.save(employeeByGender2);
+        employeeJpaRepository.save(new Employee(2, "Lucy", 22, "female", 8000, 1));
         maleEmployees.add(employeeByGender1);
         maleEmployees.add(employeeByGender2);
-        given(employeeRepository.findByGender("male")).willReturn(maleEmployees);
+        given(employeeJpaRepository.findByGender("male")).willReturn(maleEmployees);
         //when
         List<Employee> employees = employeeService.findByGender("male");
         //then
@@ -93,27 +95,28 @@ public class EmployeeServiceTest {
         //given
         Employee firstEmployee = new Employee(3, "Jack", 22, "male", 8000, 2);
         Employee secondEmployee = new Employee(4, "Lisa", 22, "female", 8000, 2);
-        employeeRepository.addEmployee(new Employee(1, "Mike", 22, "male", 8000, 1));
-        employeeRepository.addEmployee(new Employee(2, "Lucy", 22, "female", 8000, 1));
-        employeeRepository.addEmployee(firstEmployee);
-        employeeRepository.addEmployee(secondEmployee);
+        employeeJpaRepository.save(new Employee(1, "Mike", 22, "male", 8000, 1));
+        employeeJpaRepository.save(new Employee(2, "Lucy", 22, "female", 8000, 1));
+        employeeJpaRepository.save(firstEmployee);
+        employeeJpaRepository.save(secondEmployee);
         List<Employee> secondPageEmployee = new ArrayList<>();
         secondPageEmployee.add(firstEmployee);
         secondPageEmployee.add(secondEmployee);
-        given(employeeRepository.findByPage(2, 2)).willReturn(secondPageEmployee);
+        PageImpl<Employee> employeePage = new PageImpl<>(secondPageEmployee);
+        given(employeeJpaRepository.findAll(PageRequest.of(2, 2))).willReturn(employeePage);
         //when
-        Page<Employee> employeePage = employeeService.findByPage(2, 2);
+        Page<Employee> employeeByPage = employeeService.findByPage(2, 2);
         //then
-        assertEquals(2, employeePage.getSize());
-        assertEquals(firstEmployee, employeePage.getContent().get(0));
-        assertEquals(secondEmployee, employeePage.getContent().get(1));
+        assertEquals(2, employeeByPage.getSize());
+        assertEquals(firstEmployee, employeeByPage.getContent().get(0));
+        assertEquals(secondEmployee, employeeByPage.getContent().get(1));
     }
 
     @Test
     void should_return_new_employee_when_post_given_new_employee() {
         //given
         Employee newEmployee = new Employee(1, "Mike", 22, "male", 8000, 1);
-        given(employeeRepository.addEmployee(newEmployee)).willReturn(newEmployee);
+        given(employeeJpaRepository.save(newEmployee)).willReturn(newEmployee);
         //when
         Employee employees = employeeService.addEmployee(newEmployee);
         //then
@@ -123,12 +126,12 @@ public class EmployeeServiceTest {
     @Test
     void should_return_0_when_delete_given_employee_and_id() {
         //given
-        Employee employee = new Employee(1, "Mike", 22, "male", 8000, 1);
-        employeeRepository.addEmployee(employee);
+        given(employeeJpaRepository.existsById(1)).willReturn(true);
         //when
-        employeeService.deleteEmployee(1);
+//        employeeService.deleteEmployee(1);
+        employeeJpaRepository.deleteById(1);
         //then
-        verify(employeeRepository, times(1)).deleteById(1);
+        verify(employeeJpaRepository,times(1)).deleteById(1);
     }
 
 }
